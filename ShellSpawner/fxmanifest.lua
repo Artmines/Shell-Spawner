514a75b1d3593b43e419fcb098ae4f9bae1beefd
Artmines <96462463+Artmines@users.noreply.github.com>
2023-03-27 03:47:58 -0600

    Update fxmanifest

--- ShellSpawner/fxmanifest.lua
+++ ShellSpawner/fxmanifest.lua
@@ -5,6 +5,5 @@ description 'ShellSpawner'
 version '1.0.0'
 
 client_scripts {
-    'cokeshell.lua',
-   -- 'methshell.lua'    --Add your other file for another shell here
+    'shellspawner.lua'
 }
