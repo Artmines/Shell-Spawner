e8d5ae054c040fa6b2a76ba7115839ca305f15db
Artmines <96462463+Artmines@users.noreply.github.com>
2023-03-27 03:49:30 -0600

    Update Multiple Shells

--- ShellSpawner/cokeshell.lua
+++ ShellSpawner/cokeshell.lua
@@ -1,22 +1,27 @@
-local shellModel = "k4meth_shell"
-
--- Define the position and rotation of the shell
-local shellPos = vector3(1407.76, -5814.33, 321.88)
-local shellRot = vector3(0.0, 0.0, 0.0)
-
--- Function to spawn the shell
-function SpawnShell()
-  local shellObj = CreateObject(GetHashKey(shellModel), shellPos.x, shellPos.y, shellPos.z, true, true, true)
-  SetEntityRotation(shellObj, shellRot.x, shellRot.y, shellRot.z, 0, true)
-  SetEntityAsMissionEntity(shellObj, true, true)
-  FreezeEntityPosition(shellObj, true)
-end
-
--- Event handler to spawn the shell when the resource starts
-AddEventHandler("onResourceStart", function(resourceName)
-  if (GetCurrentResourceName() ~= resourceName) then
-    return
+-- Define an array of shell models, positions, and rotations
+local shellSpawnPoints = {
+    {model = "k4coke_shell", pos = vector3(1380.88, -5681.09, 307.2), rot = vector3(0.0, 0.0, 0.0)},
+    {model = "k4weed_shell", pos = vector3(-3771.15, -5796.7, 492.11), rot = vector3(0.0, 0.0, 0.0)},
+    {model = "k4meth_shell", pos = vector3(-5546.97, -1988.14, 382.11), rot = vector3(0.0, 0.0, 0.0)}
+    --{model = "prop_shell_04", pos = vector3(0.0, 0.0, 10.0), rot = vector3(0.0, 0.0, 0.0)}
+  }
+  
+  -- Function to spawn the shells
+  function SpawnShells()
+    for _, shell in ipairs(shellSpawnPoints) do
+      local shellObj = CreateObject(GetHashKey(shell.model), shell.pos.x, shell.pos.y, shell.pos.z, true, true, true)
+      SetEntityRotation(shellObj, shell.rot.x, shell.rot.y, shell.rot.z, 0, true)
+      SetEntityAsMissionEntity(shellObj, true, true)
+      FreezeEntityPosition(shellObj, true)
+    end
   end
-
-  SpawnShell()
-end)
+  
+  -- Event handler to spawn the shells when the resource starts
+  AddEventHandler("onResourceStart", function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+  
+    SpawnShells()
+  end)
+  
